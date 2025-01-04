9ea76b945763b85137a01eb6e5154a10dd6f5f12
katie1708 <katienguyen3012@gmail.com>
2025-01-04 13:58:35 -0800

    Add Sign in form without auth function

---
+++ bakerpedia-client/src/pages/Signin page/Signin.jsx
@@ -0,0 +1,23 @@
+import "./Signin.scss"
+import { Link } from "react-router-dom"
+
+export default function Signin() {
+
+    return(
+        <div className="signin">
+            <h1>My Profile</h1>
+            <form className="signin__form">
+                <div>
+                    <label htmlFor="username">Username</label>
+                    <input type="text" name="username"></input>
+                </div>
+                <div>
+                    <label htmlFor="password">Password</label>
+                    <input type="password" name="password"></input>
+                </div>
+                <button>Log in</button>
+            </form>
+            <p>Don't have an account? <Link>Sign Up</Link></p>
+        </div>
+    )
+}
